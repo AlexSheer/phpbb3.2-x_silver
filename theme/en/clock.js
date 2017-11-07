3e94d775f3b6d23f001681a4bc28c90c38e56972
AlexSheer <alex_sheer@inbox.lv>
2017-11-07 15:53:17 +0200

    Init
    Initial

---
+++ theme/en/clock.js
@@ -0,0 +1,27 @@
+	var mn = "th";
+
+// The following arrays contain data which is used in the clock's
+// date function. Feel free to change values for Days and Months
+// if needed (if you wanted abbreviated names for example).
+	var DaysOfWeek = new Array(7);
+		DaysOfWeek[0] = "Sun";
+		DaysOfWeek[1] = "Mon";
+		DaysOfWeek[2] = "Tue";
+		DaysOfWeek[3] = "Wed";
+		DaysOfWeek[4] = "Thu";
+		DaysOfWeek[5] = "Fri";
+		DaysOfWeek[6] = "Sat";
+
+	var MonthsOfYear = new Array(12);
+		MonthsOfYear[0] = "Jan";
+		MonthsOfYear[1] = "Feb";
+		MonthsOfYear[2] = "Mar";
+		MonthsOfYear[3] = "Apr";
+		MonthsOfYear[4] = "May";
+		MonthsOfYear[5] = "Jun";
+		MonthsOfYear[6] = "Jul";
+		MonthsOfYear[7] = "Aug";
+		MonthsOfYear[8] = "Sep";
+		MonthsOfYear[9] = "Oct";
+		MonthsOfYear[10] = "Nov";
+		MonthsOfYear[11] = "Dec";
---
+++ theme/ru/clock.js
@@ -0,0 +1,24 @@
+	var mn = "";
+
+	var DaysOfWeek = new Array(7);
+		DaysOfWeek[0] = "Вс";
+		DaysOfWeek[1] = "Пн";
+		DaysOfWeek[2] = "Вт";
+		DaysOfWeek[3] = "Ср";
+		DaysOfWeek[4] = "Чт";
+		DaysOfWeek[5] = "Пт";
+		DaysOfWeek[6] = "Вс";
+
+	var MonthsOfYear = new Array(12);
+		MonthsOfYear[0] = "Январь";
+		MonthsOfYear[1] = "Февраль";
+		MonthsOfYear[2] = "Март";
+		MonthsOfYear[3] = "Апрель";
+		MonthsOfYear[4] = "Май";
+		MonthsOfYear[5] = "Июнь";
+		MonthsOfYear[6] = "Июль";
+		MonthsOfYear[7] = "Август";
+		MonthsOfYear[8] = "Сентябрь";
+		MonthsOfYear[9] = "Октябрь";
+		MonthsOfYear[10] = "Ноябрь";
+		MonthsOfYear[11] = "Декабрь";
